8d5344c59d18c6fb594e45e999a1678c6ed83392
Blanca Azucena López Garduño <blankazucenalg@gmail.com>
2021-04-19 15:20:07 -0500

    test: Add testing cases for user name and email validations

--- tests/test_user.py
+++ tests/test_user.py
@@ -15,3 +15,27 @@ def test_duplicate_user(client):
     response = client.post(
         '/user', json={'name': 'Jane Doe', 'email': 'jane@example.com', 'age': 27})
     assert response.status_code == 409
+
+
+def test_no_name(client):
+    response = client.post(
+        '/user', json={'email': 'jane@example.com', 'age': 27})
+    assert response.status_code == 400
+
+
+def test_empty_name(client):
+    response = client.post(
+        '/user', json={'name': '', 'email': 'jane@example.com', 'age': 27})
+    assert response.status_code == 400
+
+
+def test_no_email(client):
+    response = client.post(
+        '/user', json={'name': 'Jane Doe', 'age': 27})
+    assert response.status_code == 400
+
+
+def test_invalid_email(client):
+    response = client.post(
+        '/user', json={'name': 'Jane Doe', 'email': 'thisisnotanemail', 'age': 27})
+    assert response.status_code == 400
